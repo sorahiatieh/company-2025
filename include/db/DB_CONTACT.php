22c404ebbba1389047d3b320bca482c598119839
atieh <a.sorahitalab@gmail.com>
2025-04-08 19:46:13 +0330

    added function addContact()

--- include/db/DB_CONTACT.php
+++ include/db/DB_CONTACT.php
@@ -32,5 +32,22 @@
 		
 		function addContact($details=array()){
 			global $mysqli;
+			
+			$fields="";
+			$values="";
+			
+			foreach($details as $key=>$value){
+				$fields.="`$key`,";
+				$values.="'$value',";
+			}
+			
+			if($fields!=""){
+				$fields=mb_substr($fields,0,mb_strlen($fields)-1);
+				$values=mb_substr($values,0,mb_strlen($values)-1);
+			}
+			
+			$q="INSERT INTO tbl_contacts($fields) VALUES($values)";
+			$mysqli->query($q);
+			return $mysqli->insert_id;
 		}
 	}
