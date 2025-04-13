643874258eeaa71b9609b1c9ec7e1d5a6ae7c23e
atieh <a.sorahitalab@gmail.com>
2025-04-13 20:39:39 +0330

    fix all bug for contact

--- include/exception/Ajax.php
+++ include/exception/Ajax.php
@@ -1,7 +1,8 @@
 <?php
 	class Ajax extends Exception{
-		public static $Status=0;
-		public static $text="";
+		public  $Status=0;
+		public  $Text="";
+		public  $LastID=0;
 		
 		public function makeOutput(){
 			unset($this->xdebug_message);
