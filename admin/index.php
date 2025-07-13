72589dd28e6d429930d73437301359fa7615baf1
atieh <a.sorahitalab@gmail.com>
2025-07-13 13:30:11 +0330

    added admin section

---
+++ admin/index.php
@@ -0,0 +1,23 @@
+<?php
+	define("_AST",1);
+	require "include/inc.php";
+	
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Admin</title>
+</head>
+<body>
+<section class="sidebar">
+	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci architecto corporis cum dignissimos hic iusto labore omnis optio provident, reiciendis sequi, suscipit totam vel. Dolorem est expedita facere numquam quasi?
+</section>
+<section class="content">
+	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dignissimos dolorem esse ipsum minima, quo repellendus sapiente soluta veritatis? Alias assumenda eveniet nisi perferendis placeat quidem quos sed similique totam!
+</section>
+</body>
+</html>
