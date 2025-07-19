4995f1f0372c4d2d7270787505f3214532efbe38
atieh <a.sorahitalab@gmail.com>
2025-07-19 20:22:09 +0330

    compelete login file

--- admin/process/login.php
+++ admin/process/login.php
@@ -12,6 +12,43 @@
 			$respons->Text="خطا در ورودی!";
 			throw $respons;
 		}
+		
+		$username=secure(trim($_POST['username']));
+		$password=secure(trim($_POST['password']));
+		
+		if($username=="" || $password==""){
+			$respons->Text="هر دو فیلد نام کاربری و رمز عبور باید تکمیل گردند!";
+			throw $respons;
+		}
+		$user_db=new DB_USER();
+		if(User::isValidUsername($username)){
+			$respons->Text="نام کاربری یا رمز عبور صحیح نمی باشد!";
+			throw $respons;
+		}
+		$UserDetails=$user_db->setWheres(array(
+			"username"=>$username
+		))->getDetails()->run();
+		
+		if(empty($UserDetails)){
+			$respons->Text="نام کاربری یا رمز عبور صحیح نمیباشد!";
+			throw $respons;
+		}
+		// پسورد رو خارج از دیتابیس چک میکنیم
+		$password=md5($password);
+		
+		if($UserDetails['password']!=$password){
+			$respons->Text="نام کاربری یا رمز عبور صحیح نمیباشد!";
+			throw $respons;
+		}
+		
+		if($UserDetails['enable']== 0){
+			$respons->Text="حساب کاربری شما توسط مدیریت غیرفعال شده است!";
+			throw $respons;
+		}
+		
+		$respons->Status=1;
+		$respons->Text="اطلاعات تایید شد.\n بزودی به ناحیه کاربری منتقل خواهید شد.";
+		throw $respons;
 	}
 	catch(Ajax $e){
 		$respons->makeOutput();
---
+++ include/db/DB_USER.php
@@ -0,0 +1,8 @@
+<?php
+	class DB_USER extends DB {
+		protected $table_name="tbl_users";
+		
+		static function isValidUsername($username) {
+			return Validator::is_az09_($username);
+		}
+	};
