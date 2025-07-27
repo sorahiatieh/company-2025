9d8e1d23857c02bdd1ed647f2000182db289b975
atieh <a.sorahitalab@gmail.com>
2025-07-27 12:46:29 +0330

    path to sidebar

--- admin/pages/require/sidebar.php
+++ admin/pages/require/sidebar.php
@@ -4,11 +4,22 @@
         <ul class="list-unstyled">
             <li>
                 <a href="?page=texts&sub=news">اخبار</a>
+            </li>
+            <li>
                 <a href="?page=texts&sub=pages">صفحات</a>
             </li>
         </ul>
     </li>
-    <li><a href="?page=products&sub=manage_products">مدیریت محصولات</a></li>
+    <li><a href="?page=products&sub=manage_products">مدیریت محصولات</a>
+        <ul class="list-unstyled">
+            <li>
+                <a href="?page=texts&sub=news">لیست محصولات</a>
+            </li>
+            <li>
+                <a href="?page=texts&sub=pages">گروه بندی</a>
+            </li>
+        </ul>
+    </li>
     <li><a href="?page=contacts&sub=manage_contacts">مدیریت پیام ها</a></li>
     <li><a href="?page=settings&sub=site">تنظیمات سایت</a></li>
     <li><a href="exit.php">خروج</a></li>
